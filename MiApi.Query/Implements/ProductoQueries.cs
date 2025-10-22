c00cdf862b0a4dbc5ddcac072b634bd6900a7027
christu12 <christian.urrego12@gmail.com>
2025-10-21 22:38:51 -0500

    Crear ProductoQueries e implementacion de metodo GetAll para mostrar la lista de productos completa

---
+++ MiApi.Query/Implements/ProductoQueries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MiApi.Models;
+using MiApi.Query.Interfaces;
+
+namespace MiApi.Query.Implements
+{
+    public class ProductoQueries : IProductoQueries
+    {
+        private readonly IDbConnection _db;
+
+        public ProductoQueries(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        public async Task<IEnumerable<Producto>> GetAll()
+        {
+            string sql = "SELECT * FROM Producto";
+            return await _db.QueryAsync<Producto>(sql);
+        }
+    }
+}
