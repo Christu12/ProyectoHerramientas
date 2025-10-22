10b837ec0c5afb8576b772e6d03ff289f06d5544
christu12 <christian.urrego12@gmail.com>
2025-10-21 22:39:35 -0500

    Indicar el metodo a utilizar para mostrar los productos

---
+++ MiApi.Query/Interfaces/IProductoQueries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MiApi.Models;
+
+namespace MiApi.Query.Interfaces
+{
+    public interface IProductoQueries
+    {
+        Task<IEnumerable<Producto>> GetAll();
+    }
+}
