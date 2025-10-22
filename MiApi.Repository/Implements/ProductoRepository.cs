8b5a474af6a7b5fe01a84a3a341a881e428aec8b
christu12 <christian.urrego12@gmail.com>
2025-10-21 22:40:29 -0500

    Implementar metodos de CUD para la tabla Producto en la base de datos

---
+++ MiApi.Repository/Implements/ProductoRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using MiApi.Models;
+using MiApi.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiApi.Repository.Implements
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly IDbConnection _db;
+
+        public ProductoRepository(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public async Task<int> Add(Producto p)
+        {
+            var id = await _db.InsertAsync(p);
+            return id;
+        }
+        public async Task<bool> Update(Producto p)
+        {
+           return await _db.UpdateAsync(p);
+           
+        }
+        public async Task<bool> Delete(int id)
+        {
+            var producto = await _db.GetAsync<Producto>(id);
+            if (producto == null) return false;
+            return await _db.DeleteAsync(producto);
+        }
+    }
+}
