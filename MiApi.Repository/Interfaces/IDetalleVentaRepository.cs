d287454ae5f3027a665e310b8976cd5d42d1d427
christu12 <christian.urrego12@gmail.com>
2025-10-22 22:57:03 -0500

    agregar interfaces IVentaRepository e IDetalleVentaRepository

---
+++ MiApi.Repository/Interfaces/IDetalleVentaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MiApi.Models;
+
+namespace MiApi.Repository.Interfaces
+{
+    public interface IDetalleVentaRepository
+    {
+        Task<int> Add(DetalleVenta d);
+        Task<bool> Update(DetalleVenta d);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ MiApi.Repository/Interfaces/IVentaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MiApi.Models;
+
+namespace MiApi.Repository.Interfaces
+{
+    public interface IVentaRepository
+    {
+        Task<int> Add(Venta v);
+    }
+}
