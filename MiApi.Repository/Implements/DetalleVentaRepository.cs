6020100b9c8520ec6ac6a9cd607c80a6b0ddc7d6
christu12 <christian.urrego12@gmail.com>
2025-10-22 22:56:27 -0500

    implementación completa de VentaRepository y DetalleVentaRepository

---
+++ MiApi.Repository/Implements/DetalleVentaRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MiApi.Models;
+using MiApi.Repository.Interfaces;
+
+namespace MiApi.Repository.Implements
+{
+    public class DetalleVentaRepository : IDetalleVentaRepository
+    {
+        private readonly IDbConnection _db;
+
+        public DetalleVentaRepository(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public async Task<int> Add(DetalleVenta d)
+        {
+            try
+            {
+                var id = await _db.InsertAsync(d);
+                return id;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                await _db.ExecuteAsync("DELETE FROM DetalleVenta WHERE IdDetalle = @id", new { id });
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Update(DetalleVenta d)
+        {
+            try
+            {
+                return await _db.UpdateAsync(d);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ MiApi.Repository/Implements/VentaRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using MiApi.Models;
+using MiApi.Repository.Interfaces;
+
+namespace MiApi.Repository.Implements
+{
+    public class VentaRepository : IVentaRepository
+    {
+        private readonly IDbConnection _db;
+        public VentaRepository(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        public async Task<int> Add(Venta v)
+        {
+            try
+            {
+                var id = await _db.InsertAsync(v);
+                return (int)id;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al registrar la venta", ex);
+            }
+        }
+    }
+}
