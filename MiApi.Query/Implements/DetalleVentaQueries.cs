2ad801c541540a932cdf881dc5f262c733d19b11
christu12 <christian.urrego12@gmail.com>
2025-10-22 22:52:38 -0500

    agregar clases VentaQueries y DetalleVentaQueries para manejo de consultas SQL

---
+++ MiApi.Query/Implements/DetalleVentaQueries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MiApi.Models;
+using MiApi.Query.Interfaces;
+
+
+namespace MiApi.Query.Implements
+{
+    public class DetalleVentaQueries : IDetalleVentaQueries
+    {
+        private readonly IDbConnection _db;
+
+        public DetalleVentaQueries(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        public async Task<IEnumerable<DetalleVenta>> GetAll()
+        {
+            var sql = "SELECT * FROM DetalleVenta";
+            return await _db.QueryAsync<DetalleVenta>(sql);
+        }
+
+        public Task<DetalleVenta?> GetById(int id)
+        {
+            var sql = "SELECT * FROM DetalleVenta WHERE IdDetalle = @id";
+            return _db.QueryFirstOrDefaultAsync<DetalleVenta>(sql, new { id });
+        }
+    }
+}
---
+++ MiApi.Query/Implements/VentaQueries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MiApi.Models;
+using MiApi.Query.Interfaces;
+
+namespace MiApi.Query.Implements
+{
+    public class VentaQueries : IVentaQueries
+    {
+        private readonly IDbConnection _db;
+
+        public VentaQueries(IDbConnection db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        public async Task<IEnumerable<Venta>> GetAll()
+        {   
+            string sql = "SELECT * FROM Venta";
+            return await _db.QueryAsync<Venta>(sql);
+        }
+        public async Task<Venta?> GetById(int id)
+        {
+            string sql = "SELECT * FROM Venta WHERE IdVenta = @id";
+            return await _db.QueryFirstOrDefaultAsync<Venta>(sql, new { id });
+        }
+    }
+}
+
