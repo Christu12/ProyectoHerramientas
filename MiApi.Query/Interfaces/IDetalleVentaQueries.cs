ecd84d508cc3e8453842736d2500e89ff0544109
christu12 <christian.urrego12@gmail.com>
2025-10-22 22:53:54 -0500

    agregar interfaces IVentaQueries e IDetalleVentaQueries

---
+++ MiApi.Query/Interfaces/IDetalleVentaQueries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MiApi.Models;
+
+namespace MiApi.Query.Interfaces
+{
+    public interface IDetalleVentaQueries
+    {
+        Task<IEnumerable<DetalleVenta>> GetAll();
+        Task<DetalleVenta> GetById(int id);
+    }
+}
---
+++ MiApi.Query/Interfaces/IVentaQueries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MiApi.Models;
+
+namespace MiApi.Query.Interfaces
+{
+    public interface IVentaQueries
+    {
+        Task<IEnumerable<Venta>> GetAll();
+        Task<Venta?> GetById(int id);     
+    }
+}
